dcea0a81100c66c96ce6ebc4dba8509ed2b5a942
nate4t 5 <wumboToken@gmail.com>
2022-01-03 14:22:45 -0800

    initial commit

---
+++ src/components/wallet.js
@@ -0,0 +1,16 @@
+import { React, useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import styles from './Wallet.module.css';
+import simple_token_abi from './Contracts/simple_token_abi.json'
+import Interactions from './Interactions';
+
+
+function Wallet() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Wallet
